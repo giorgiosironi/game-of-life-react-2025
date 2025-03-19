94b4701a23b610dbd5414579fc1fbdb30f746ebb
Giorgio Sironi <work@giorgiosironi.com>
2025-03-19 15:00:10 +0000

    Provide current state and affordance to switch between generations

--- src/components/WorldWindow.tsx
+++ src/components/WorldWindow.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 type Props = {
-  generation: string;
+  currentGeneration: string;
 }
 
 const emptyGrid = () => Array(6).fill(null).map(() => Array(8).fill(false));
 
-const WorldWindow: React.FC<Props> = ({ generation }) => {
+const WorldWindow: React.FC<Props> = ({ currentGeneration }) => {
   const only_horizontal_blinker = emptyGrid();
   only_horizontal_blinker[2][2] = true;
   only_horizontal_blinker[2][3] = true;
@@ -17,20 +17,27 @@ const WorldWindow: React.FC<Props> = ({ generation }) => {
   only_vertical_blinker[2][3] = true;
   only_vertical_blinker[3][3] = true;
 
-  const grid = generation === '2' ? only_vertical_blinker : only_horizontal_blinker;
+  const grid = currentGeneration === '2' ? only_vertical_blinker : only_horizontal_blinker;
+  const otherGeneration = currentGeneration === '2' ? '1' : '2';
 
   return (
-    <table>
-      {grid.map((row) => (
-        <tr>
-          {row.map((isAlive) => (
-            <td 
-              className={isAlive ? 'cell--alive' : 'cell--dead'}
-            />
-          ))}
-        </tr>
-      ))}
-    </table>
+    <div>
+      <h1>Generation {currentGeneration}</h1>
+      <table>
+        {grid.map((row) => (
+          <tr>
+            {row.map((isAlive) => (
+              <td 
+                className={isAlive ? 'cell--alive' : 'cell--dead'}
+              />
+            ))}
+          </tr>
+        ))}
+      </table>
+      <a href={`/examples/blinker?generation=${otherGeneration}`}>
+        Switch to generation {otherGeneration}
+      </a>
+    </div>
   );
 };
 
--- src/server.tsx
+++ src/server.tsx
@@ -23,10 +23,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/examples/blinker', (req, res) => {
-  const generation = (req.query.generation ?? 1) as string;
+  const currentGeneration = (req.query.generation ?? 1) as string;
   const html = ReactDOMServer.renderToString(
     <WorldWindow
-      generation={generation}
+      currentGeneration={currentGeneration}
     />
   );
   
